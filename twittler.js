afc7b140a71d45c34269912aca47bbd77482c818
George gd Fekaris <gdfekaris@gmail.com>
2018-07-10 16:32:38 -0700

    Not there yet, but on the way.

--- twittler.js
+++ twittler.js
@@ -11,18 +11,39 @@ $(document).ready(function(){
     while(index >= 0){
       let tweet = streams.home[index];
       let $tweet = $(`<div class="tweets"></div>`);
-      $tweet.text(`@${tweet.user}: ${tweet.message} ${tweet.created_at}`);
+      let userButton = `<button class="userButton">@${tweet.user}</button>`;
+      $tweet.html(`${userButton}: ${tweet.message} ${tweet.created_at}`);
       $tweet.appendTo($tweetList);
       index -= 1;
     }
   };
 
+   const redirect = (x) => {
+    // for (var key in streams.users) {
+    // if (userbuttonname === streams.users[key])
+    //   return streams.users[key]
+    // }
+    // let index = streams.users.shawndrost.length - 1;
+    // $tweetList.html('');
+    // while(index >= 0){
+    //   let tweet = streams.users.shawndrost[index];
+    //   let $tweet = $(`<div class="tweets"></div>`);
+    //   let userButton = `<button class="userButton">@${tweet.user}</button>`;
+    //   $tweet.html(`${userButton}: ${tweet.message} ${tweet.created_at}`);
+    //   $tweet.appendTo($tweetList);
+    //   index -= 1;
+    // }
 
+    
+    //return alert('this button works');
+   }
   //$(event.target)
   //$(
  
 
-  $("#newTweets").on('click', tweetRefresh);
+  jQuery("#newTweets").on('click', tweetRefresh); //number.toString()
   //setInterval(tweetRefresh, 3000);
+
+  jQuery("body").on('click', '.userButton', redirect);
 
 });
